f9970f4151cb0bc02590942ddf8ad0f69f165b31
Eugeniusz Neugebauer <eugeniusz.neugebauer@telia.no>
2022-04-01 13:37:34 +0200

    Added C3P0 support

--- spring-boot-rest-jpa/src/main/java/pl/ene/springbootrestjpa/SpringBootRestJpaApplication.java
+++ spring-boot-rest-jpa/src/main/java/pl/ene/springbootrestjpa/SpringBootRestJpaApplication.java
@@ -5,13 +5,16 @@ import java.sql.SQLException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
+import pl.ene.springbootrestjpa.config.C3P0DataSourceProperties;
 import pl.ene.springbootrestjpa.config.PersistenceConfig;
 
 //@EnableAutoConfiguration
 @SpringBootApplication
 @Import(PersistenceConfig.class)
+//@EnableConfigurationProperties(C3P0DataSourceProperties.class)
 public class SpringBootRestJpaApplication {
 
 	public static void main(String[] args) throws SQLException {
--- spring-boot-rest-jpa/src/main/java/pl/ene/springbootrestjpa/config/PersistenceConfig.java
+++ spring-boot-rest-jpa/src/main/java/pl/ene/springbootrestjpa/config/PersistenceConfig.java
@@ -1,12 +1,28 @@
 package pl.ene.springbootrestjpa.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 public class PersistenceConfig {
 
-//c1
-//c2
-	
+@Bean
+public DataSource dataSource(C3P0DataSourceProperties dataSourcePros) throws PropertyVetoException {
+
+    ComboPooledDataSource  pooledDataSource = new ComboPooledDataSource();
+    pooledDataSource.setDriverClass(dataSourcePros.getDriverClass());
+    pooledDataSource.setUser(dataSourcePros.getUsername());
+    pooledDataSource.setPassword(dataSourcePros.getPassword());
+    pooledDataSource.setJdbcUrl(dataSourcePros.getUrl());
+    pooledDataSource.setMaxPoolSize(dataSourcePros.getMaxPoolSize());
+    pooledDataSource.setMaxIdleTime(dataSourcePros.getMaxIdleTime());
+
+    return pooledDataSource;
+}
+
 }
