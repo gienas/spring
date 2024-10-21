c55352f3dfaa9cb97b5771b9126ec282d1f4210f
gienas <gienas@gmail.com>
2024-10-21 16:40:09 +0200

    Spring boot upgrade + error handling

--- spring-boot-rest-jpa/src/main/java/pl/ene/springbootrestjpa/controllers/PetStoreController.java
+++ spring-boot-rest-jpa/src/main/java/pl/ene/springbootrestjpa/controllers/PetStoreController.java
@@ -3,9 +3,13 @@ package pl.ene.springbootrestjpa.controllers;
 import org.openapitools.client.api.PetsApi;
 import org.openapitools.client.model.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import pl.ene.springbootrestjpa.domain.Greeting;
 import pl.ene.springbootrestjpa.domain.Person;
 
@@ -22,8 +26,17 @@ public class PetStoreController {
 	}
 
 	@RequestMapping("/pets")
-	public List<Pet> pets() {
-		return petStoreApi.getPetsPetsGet();
+	public ResponseEntity<?> pets() {
+		try {
+			List<Pet> pets = petStoreApi.getPetsPetsGet();
+			return ResponseEntity.ok(pets);
+		} catch (HttpClientErrorException e) {
+			return ResponseEntity.status(e.getStatusCode()).body("Client error: " + e.getMessage());
+		} catch (HttpServerErrorException e) {
+			return ResponseEntity.status(e.getStatusCode()).body("Server error: " + e.getMessage());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred: " + e.getMessage());
+		}
 	}
 
 
